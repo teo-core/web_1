e4dfb2329448eee47956b3382603825da1d69ab6
Teo <tsanchez@corenetworks.es>
2022-03-28 10:57:06 +0200

    ocupaciones

--- app.py
+++ app.py
@@ -15,7 +15,7 @@ def server_static(filename):
 @jinja2_view('home.html')
 def hola():
     cnx = sqlite3.connect(BASE_DATOS)
-    consulta = "select id,nombre, apelllidos,dni from persona"
+    consulta = "select id,nombre, apelllidos,dni, id_ocupacion from persona"
     cursor = cnx.execute(consulta)
     filas = cursor.fetchall()
     cnx.close()
@@ -25,15 +25,20 @@ def hola():
 @route('/editar/<id:int>')
 @jinja2_view('formulario.html')
 def mi_form(id=None):
+    cnx = sqlite3.connect(BASE_DATOS)
+    consulta = "select * from T_ocupacion"
+    cursor = cnx.execute(consulta)
+    ocupaciones = cursor.fetchall()
+
     if id is None:
-        return {}
+        return {'ocupaciones':ocupaciones}
     else:
-        cnx = sqlite3.connect(BASE_DATOS)
-        consulta = "select id,nombre, apelllidos,dni from persona where id =?"
+        consulta = "select id,nombre, apelllidos,dni, id_ocupacion from persona where id =?"
         cursor = cnx.execute(consulta,(id,))
         filas = cursor.fetchone()
-        cnx.close()
-        return {'datos': filas}
+
+    cnx.close()
+    return {'datos': filas,'ocupaciones':ocupaciones}
 
 @route('/guardar', method='POST')
 def guardar():
@@ -41,21 +46,29 @@ def guardar():
     apellidos = request.POST.apellidos
     dni = request.POST.dni
     id = request.POST.id
+    ocupacion = request.POST.ocupacion
     
     cnx = sqlite3.connect(BASE_DATOS)
     
     if id =='': #Alta
-        consulta = "insert into persona(nombre, apelllidos,dni) values (?,?,?)"
-        cnx.execute(consulta,(nombre,apellidos,dni))
+        consulta = "insert into persona(nombre, apelllidos,dni, id_ocupacion) values (?,?,?,?)"
+        cnx.execute(consulta,(nombre,apellidos,dni,ocupacion))
     else: #Actualización
-        consulta = "update persona set nombre = ?, apelllidos = ?, dni =? where id =?"
-        cnx.execute(consulta,(nombre,apellidos,dni,id))
+        consulta = "update persona set nombre = ?, apelllidos = ?, dni =?, id_ocupacion=? where id =?"
+        cnx.execute(consulta,(nombre,apellidos,dni,ocupacion,id))
 
     cnx.commit()
     cnx.close()
     redirect('/')
 
-
+@route('/borrar/<id:int>')
+def borrar(id):
+    cnx = sqlite3.connect(BASE_DATOS)
+    consulta = f'delete from persona where id="{id}"'
+    cnx.execute(consulta) 
+    cnx.commit()
+    cnx.close()
+    redirect('/')
 
 
 run(host= 'localhost',port=8080, debug=True)
